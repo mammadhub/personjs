d1672e664c8e79f2725e7745899fd8cf31a34b61
mammad <memmed2512@gmail.com>
2024-04-26 23:52:24 +0400

    personjs

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 function A(persons, name) {
     const person = persons.find(person => person.name.toLowerCase() === name.toLowerCase());
     if (!person) {
-        console.log("Bilet al.");
-        return;
+        return "Bilet al.";
     }
-
-    if (person.age < 18) {
-        console.log(`${person.name} böyəl gəl.`);
-    } else {
-        console.log(`${person.name} buyur.`);
+    else {
+        if (person && person.age < 18) {
+            return `${person.name} böyəl gəl.`;
+        } else {
+            return `${person.name} buyur.`;
+        }
     }
 }
 
 let persons = [
     {
-    name: "Mike",
-    age: 12,
-    gender: "male"
-    },{
-    name: "Madeline",
-    age: 80,
-    gender: "female"
-    },{
-    name: "Cheryl",
-    age: 22,
-    gender: "female"
-    },{
-    name: "Sam",
-    age: 30,
-    gender: "male"
-    },{
-    name: "Suzy",
-    age: 4,
-    gender: "female"
+        name: "Mike",
+        age: 12,
+        gender: "male"
+    }, {
+        name: "Madeline",
+        age: 80,
+        gender: "female"
+    }, {
+        name: "Cheryl",
+        age: 22,
+        gender: "female"
+    }, {
+        name: "Sam",
+        age: 30,
+        gender: "male"
+    }, {
+        name: "Suzy",
+        age: 4,
+        gender: "female"
     }
-    ];
-    
-A(persons, "mike");
-A(persons, "cheryl"); 
-A(persons, "Ali");
+];
+
+console.log(A(persons, "mike")); 
+console.log( A(persons, "cheryl"));
+console.log(A(persons, "Ali")); 
